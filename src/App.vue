53d6ff77c71a7a902f7806a2cfa328591975dbec
Paul N <avedomaru@gmail.com>
2019-02-23 17:12:28 +0300

    vue + vuetify + electron app

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app id="app">
+    <v-navigation-drawer v-model="primaryDrawer" :clipped="true" absolute overflow app></v-navigation-drawer>
+
+    <v-toolbar clipped-left app absolute>
+      <v-toolbar-side-icon @click.stop="primaryDrawer = !primaryDrawer"/>
+      <v-toolbar-title>Title</v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    primaryDrawer: null
+  })
+};
+</script>
+
+<style lang="scss">
+.desktop-menu {
+  margin-top: 58px;
+}
+</style>
